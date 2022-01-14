9640f4e696951d53a5435b22e27f626ef07322c4
KChi <59391215+KagChi@users.noreply.github.com>
2022-01-14 10:00:31 +0700

    feat(*): initial commit

--- README.md
+++ README.md
@@ -2,10 +2,21 @@
 
 ![Kirishima Banner](https://cdn.discordapp.com/attachments/891939988088975372/931079377771450388/kirishima-ship-banner.png)
 
-# @kirishima/template
+# @kirishima/collection
 
 </div>
 
 # Features
-- lorem
-- ipsum
+- Written in TypeScript
+- Asynchronous 
+
+# Example
+```ts
+import { KirishimaCollection } from "@kirishima/collection";
+(async () => {
+    const collection = new KirishimaCollection();
+    await collection.set("key", "value");
+    const savedData = await collection.get("key");
+    console.log(savedData);
+})()
+```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,59 @@
-export const main = () => 'Kirishima ship ready to serve !';
+export class KirishimaCollection<K, V> extends Map<K, V> {
+	/* @ts-expect-error temporary ignore, pr if you have solutions. */
+	public set(key: K, value: V): Promise<this> {
+		return new Promise((resolve) => {
+			return resolve(super.set(key, value));
+		});
+	}
 
-export default main;
+	/* @ts-expect-error temporary ignore, pr if you have solutions. */
+	public get(key: K): Promise<V | undefined> {
+		return new Promise((resolve) => {
+			return resolve(super.get(key));
+		});
+	}
+
+	public clear(): Promise<void> {
+		return new Promise((resolve) => {
+			return resolve(super.clear());
+		});
+	}
+
+	/* @ts-expect-error temporary ignore, pr if you have solutions. */
+	public delete(key: K): Promise<boolean> {
+		return new Promise((resolve) => {
+			return resolve(super.delete(key));
+		});
+	}
+
+	/* @ts-expect-error temporary ignore, pr if you have solutions. */
+	public has(key: K): Promise<boolean> {
+		return new Promise((resolve) => {
+			return resolve(super.has(key));
+		});
+	}
+
+	public last(): Promise<V | undefined>;
+	public last(amount: number): Promise<V[]>;
+	public last(amount?: number): Promise<V | V[] | undefined> {
+		return new Promise((resolve) => {
+			const arr = [...this.values()];
+			if (typeof amount === 'undefined') return resolve(arr[arr.length - 1]);
+			if (amount < 0) return resolve(this.first(amount * -1));
+			if (!amount) return [];
+			return resolve(arr.slice(-amount));
+		});
+	}
+
+	public first(): Promise<V | undefined>;
+	public first(amount: number): Promise<V[]>;
+	public first(amount?: number): Promise<V | V[] | undefined> {
+		return new Promise((resolve) => {
+			if (typeof amount === 'undefined') return this.values().next().value;
+			if (amount < 0) return this.last(amount * -1);
+			amount = Math.min(this.size, amount);
+			const iter = this.values();
+			return resolve(Array.from({ length: amount }, (): V => iter.next().value));
+		});
+	}
+}
